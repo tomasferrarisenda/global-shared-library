cc6ad97df3c1e7948cd302f8ab01a9fc2593e33f
Tomas Ferrari <tferrari.92@gmail.com>
2024-05-30 00:34:44 +0200

    asdas

--- vars/updateValues.groovy
+++ vars/updateValues.groovy
@@ -1,20 +1,20 @@
 def updateDev(Map config = [:]) {
     sh """
-        sed "s/tag:.*/tag: ${config.tag}/g" helm-chart/environments/values-dev.yaml > values-dev.temp
-        mv values-dev.temp helm-chart/environments/values-dev.yaml
+        sed "s/tag:.*/tag: ${config.tag}/g" environments/values-dev.yaml > values-dev.temp
+        mv values-dev.temp environments/values-dev.yaml
     """
 }
 
 def updateStage(Map config = [:]) {
     sh """
-        sed "s/tag:.*/tag: ${config.tag}/g" helm-chart/environments/values-stage.yaml > values-stage.temp
-        mv values-stage.temp helm-chart/environments/values-stage.yaml
+        sed "s/tag:.*/tag: ${config.tag}/g" environments/values-stage.yaml > values-stage.temp
+        mv values-stage.temp environments/values-stage.yaml
     """
 }
 
 def updateProd(Map config = [:]) {
     sh """
-        sed "s/tag:.*/tag: ${config.tag}/g" helm-chart/environments/values-prod.yaml > values-prod.temp
-        mv values-prod.temp helm-chart/environments/values-prod.yaml
+        sed "s/tag:.*/tag: ${config.tag}/g" environments/values-prod.yaml > values-prod.temp
+        mv values-prod.temp environments/values-prod.yaml
     """
 }
