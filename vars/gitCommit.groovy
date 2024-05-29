40af558af34ad53b6064a0849e76e95d8407c0e3
Tomas Ferrari <tferrari.92@gmail.com>
2024-05-29 21:28:24 +0200

    sdcds

--- vars/gitCommit.groovy
+++ vars/gitCommit.groovy
@@ -1,11 +1,12 @@
 
 def commitAndPush(Map config = [:]) {
-    sh "tree"
+    sh "ls"
     sh "git config --global user.email ${config.email}"
     sh "git config --global user.name ${config.organization}"
     sh "git add ."
     sh "git commit -m 'Update image tags'"
     withCredentials([usernamePassword(credentialsId: "github", passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-        sh("git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${config.organization}/${config.service}.git")
+        sh("git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${config.service}.git")
     }
 }
+        // sh("git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${config.organization}/${config.service}.git")
