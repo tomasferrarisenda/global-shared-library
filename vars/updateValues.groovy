8e91b82854973f80b89cc76e3306bb5cf2ee63f2
Tomas Ferrari <tferrari.92@gmail.com>
2024-05-29 21:19:40 +0200

    fixes

--- vars/updateValues.groovy
+++ vars/updateValues.groovy
@@ -6,11 +6,13 @@
 // }
 def updateDev(Map config = [:]) {
     sh """
-        sed "s/tag:.*/tag: ${config.tag}/g" helm-charts/systems/${config.system}/${config.service}/environments/values-dev.yaml > values-dev.temp
-        mv values-dev.temp helm-charts/systems/${config.system}/${config.service}/environments/values-dev.yaml
+        sed "s/tag:.*/tag: ${config.tag}/g" helm-chart/environments/values-dev.yaml > values-dev.temp
+        mv values-dev.temp helm-chart/environments/values-dev.yaml
     """
 }
 
+        // sed "s/tag:.*/tag: ${config.tag}/g" helm-charts/systems/${config.system}/${config.service}/environments/values-dev.yaml > values-dev.temp
+        // mv values-dev.temp helm-charts/systems/${config.system}/${config.service}/environments/values-dev.yaml
 
 
 def updateStage(String tag) {
